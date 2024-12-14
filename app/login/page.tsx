6b9fee862d1101da348c5ef05e8de92555ba22c4
volant97 <volant97@naver.com>
2024-12-14 16:48:17 +0900

    [UPDATE] 0.1.7 loginpage : input과 useState 연결

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,92 @@
+"use client";
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(nickname, email, password);
+
+    if (nickname && email && password) {
+      console.log("로그인 성공!");
+    } else {
+      console.log("모든 필드를 입력하세요.");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col justify-center items-center gap-3 w-full h-3/5"
+    >
+      <label className="input input-bordered flex items-center gap-2 w-4/5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-4 w-4 opacity-70"
+        >
+          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+        </svg>
+        <input
+          type="text"
+          value={nickname}
+          onChange={(e) => {
+            setNickname(e.target.value);
+          }}
+          className="grow"
+          placeholder="닉네임"
+        />
+      </label>
+      <label className="input input-bordered flex items-center gap-2 w-4/5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-4 w-4 opacity-70"
+        >
+          <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+          <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+        </svg>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          className="grow"
+          placeholder="이메일"
+        />
+      </label>
+      <label className="input input-bordered flex items-center gap-2 w-4/5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-4 w-4 opacity-70"
+        >
+          <path
+            fillRule="evenodd"
+            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="grow"
+          placeholder="*******"
+        />
+      </label>
+      <button type="submit" className="size-12 border-2">
+        확인
+      </button>
+    </form>
+  );
 }
