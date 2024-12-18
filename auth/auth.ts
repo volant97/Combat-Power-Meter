2ad9fba722007b38a401d9749700b8f00d2fc8ae
volant97 <volant97@naver.com>
2024-12-18 15:48:24 +0900

    [UPDATE] 0.1.11 supabase 연결중중

---
+++ auth/auth.ts
@@ -0,0 +1,15 @@
+import { supabase } from "@/lib/supabase-client";
+
+/**유저 정보 확인 */
+export const getUser = async () => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user;
+};
+
+/**로그아웃 */
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) console.error("로그아웃 중 에러가 발생하였습니다.", error);
+}
